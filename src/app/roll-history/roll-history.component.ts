b203d695e12d8bb22bd86687f66530daaa3d4e5b
tpetts <tpetts@madisoncollege.edu>
2021-10-28 12:00:55 -0500

    Adding a constructor property
    importing another service

--- src/app/roll-history/roll-history.component.ts
+++ src/app/roll-history/roll-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RollHistoryService } from '../roll-history.service';
 
 @Component({
   selector: 'app-roll-history',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RollHistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public rHistorySvc: RollHistoryService
+  ) { }
 
   ngOnInit(): void {
   }
