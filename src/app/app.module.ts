eb45c745294fae863a73770c8fd5571fe368b519
tpetts <tpetts@madisoncollege.edu>
2021-10-28 10:53:12 -0500

    ng generate component die-roller --skip-tests
    generates an html, .ts. .css files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DieRollerComponent } from './die-roller/die-roller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DieRollerComponent
   ],
   imports: [
     BrowserModule
