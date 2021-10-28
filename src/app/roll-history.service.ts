7812fa412504d0318486468ba0a8ce2fbe8aa8bf
tpetts <tpetts@madisoncollege.edu>
2021-10-28 11:53:30 -0500

    updating the rollhistory service
    adding in a function to add the histories of rolling

--- src/app/roll-history.service.ts
+++ src/app/roll-history.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class RollHistoryService {
 
   constructor() { }
+
+  rollHistory: any[] = [];
+
+  addRollHistory = (rHistory: any) => {
+    this.rollHistory = [
+      ...this.rollHistory
+      , rHistory
+    ];
+  };
 }
